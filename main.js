c1c5853fb9e084d5d2f3daa277f8ba8b509c164a
Adam Plocieniak <adam.plocieniak@allegro.pl>
2019-10-12 22:05:15 +0200

    inline args rather than json

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
 const request = require('request');
 
-const [, , cs, hti] = process.argv;
+const [, , cs, product, price, oldPrice, image, offerUrl ] = process.argv;
 
 console.log(cs)
 console.log(hti)
 
 const channelSecret = cs;
-
-const hotShotItem = JSON.parse(hti) || {
-    product: 'Honor Watch Magic czarny',
-    oldPrice: '699,00 zł',
-    price: '399,00 zł',
-    image: 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2019/4/pr_2019_4_29_12_29_11_213_05.jpg',
-    offerUrl: 'https://www.x-kom.pl/goracy_strzal/19526'
-};
-
-const { product, price, oldPrice, image, offerUrl } = hotShotItem
 
 request.post({
     url: 'https://hooks.slack.com/services/' + channelSecret,
